d4e1e25dff94a28a4bdbc5951557ed244e42a2d3
chumicat <russell.tseng@104.com.tw>
2020-04-07 09:11:58 +0800

    PDO to root

--- src/Logger.php
+++ src/Logger.php
@@ -6,20 +6,43 @@ use Psr\Log\LoggerInterface;
 
 class Logger implements LoggerInterface
 {
-    
-    // initial sqlite3
-    function __construct() {
-        $pdo = new PDO('sqlite:practicedb.db');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->exec("CREATE TABLE IF NOT EXISTS foo (id INTEGER PRIMARY KEY AUTOINCREMENT, level VARCHAR(10) NOT NULL, message TEXT NOT NULL)");
+    private $pdo;
+
+    /**
+     * Initial sqlite3 with table "logs" in $this->pdo
+     * 
+     * logs = 
+     * * id INTEGER PRIMARY KEY AUTOINCREMENT
+     * * level VARCHAR(10) NOT NULL
+     * * message TEXT NOT NULL
+     */
+    public function __construct() {
+        $this->pdo = new \PDO('sqlite:syslog.sqlite3');
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY AUTOINCREMENT, level VARCHAR(10) NOT NULL, message TEXT NOT NULL)");
     }
-    // Every function doing the same thing!
-    // field 'id' will increase automatically
-    // insert function name to field 'level'
-    // insert message $msg to field 'message'
-    function debug(string $msg) {}
-    function info(string $msg) {}
-    function notice(string $msg) {}
-    function critical(string $msg) {}
-    function error(string $msg) {}
+
+    /**
+     * Insert values into table "logs"
+     * 
+     * This function will be called by other function to insert values
+     * Field "id" didn't need to fill value since it increase automatically
+     * 
+     * @param string $lv value insert to field "level"
+     * @param string $msg value insert to field "message"
+     * @return void
+     */
+    private function insert(string $lv, string $msg): void
+    {
+        $stmt = $this->pdo->prepare("INSERT INTO logs (level, message) VALUES (:lv, :msg)");
+        $stmt->bindValue(':lv', $lv);
+        $stmt->bindValue(':msg', $msg);
+        $stmt->execute();
+    }
+
+    public function debug(string $msg) {$this->insert('debug', $msg);}
+    public function info(string $msg) {$this->insert('info', $msg);}
+    public function notice(string $msg) {$this->insert('notice', $msg);}
+    public function critical(string $msg) {$this->insert('critical', $msg);}
+    public function error(string $msg) {$this->insert('error', $msg);}
 }
