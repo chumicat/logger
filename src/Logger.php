10db4daf9fd075f054cbdb5849b06990bb3e9aef
chumicat <russell.tseng@104.com.tw>
2020-04-07 10:25:19 +0800

    replace insert with log

--- src/Logger.php
+++ src/Logger.php
@@ -20,31 +20,18 @@ class Logger implements LoggerInterface
         $this->pdo->exec("CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY AUTOINCREMENT, level VARCHAR(10) NOT NULL, message TEXT NOT NULL)");
     }
 
-    /**
-     * Insert values into table "logs"
-     * 
-     * This function will be called by other function to insert values
-     * Field "id" didn't need to fill value since it increase automatically
-     * 
-     * @param string $lv value insert to field "level"
-     * @param string $msg value insert to field "message"
-     * @return void
-     */
-    private function insert(string $lv, string $msg): void
-    {
+    public function emergency($message, array $context = []) {$this->log('emergency', $message);}
+    public function alert($message, array $context = []) {$this->log('alert', $message);}
+    public function critical($message, array $context = []) {$this->log('critical', $message);}
+    public function error($message, array $context = []) {$this->log('error', $message);}
+    public function warning($message, array $context = []) {$this->log('warning', $message);}
+    public function notice($message, array $context = []) {$this->log('notice', $message);}
+    public function info($message, array $context = []) {$this->log('info', $message);}
+    public function debug($message, array $context = []) {$this->log('debug', $message);}
+    public function log($level, $message, array $context = []) {
         $stmt = $this->pdo->prepare("INSERT INTO logs (level, message) VALUES (:lv, :msg)");
-        $stmt->bindValue(':lv', $lv);
-        $stmt->bindValue(':msg', $msg);
+        $stmt->bindValue(':lv', $level);
+        $stmt->bindValue(':msg', $message);
         $stmt->execute();
     }
-
-    public function emergency($message, array $context = []) {$this->insert('emergency', $message);}
-    public function alert($message, array $context = []) {$this->insert('alert', $message);}
-    public function critical($message, array $context = []) {$this->insert('critical', $message);}
-    public function error($message, array $context = []) {$this->insert('error', $message);}
-    public function warning($message, array $context = []) {$this->insert('warning', $message);}
-    public function notice($message, array $context = []) {$this->insert('notice', $message);}
-    public function info($message, array $context = []) {$this->insert('info', $message);}
-    public function debug($message, array $context = []) {$this->insert('debug', $message);}
-    public function log($level, $message, array $context = []) {}
 }
