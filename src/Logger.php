9550f70f5ba8650ccac107746ddf1f21d98bd680
chumicat <russell.tseng@104.com.tw>
2020-04-07 09:00:59 +0800

    test

--- src/Logger.php
+++ src/Logger.php
@@ -2,10 +2,17 @@
 namespace Php\Exam;
 use Psr\Log\LoggerInterface;
 
+
+
 class Logger implements LoggerInterface
 {
+    
     // initial sqlite3
-    function __construct() {}
+    function __construct() {
+        $pdo = new PDO('sqlite:practicedb.db');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->exec("CREATE TABLE IF NOT EXISTS foo (id INTEGER PRIMARY KEY AUTOINCREMENT, level VARCHAR(10) NOT NULL, message TEXT NOT NULL)");
+    }
     // Every function doing the same thing!
     // field 'id' will increase automatically
     // insert function name to field 'level'
